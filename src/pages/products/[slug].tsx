5bbee08c35f3382414b778afc132793eaf9b3f7e
php1301 <19520854@gm.uit.edu.vn>
2021-07-29 22:26:47 +0700

    dynamic product page added

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import Container from "@components/ui/container";
+import { Layout } from "@components/layout/layout";
+import { Subscription } from "@components/common/subscription";
+// import ProductSingleDetails from "@components/product/product-single-details";
+// import RelatedProducts from "@containers/related-products";
+import Divider from "@components/ui/divider";
+import Breadcrumb from "@components/common/breadcrumb";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { GetServerSideProps } from "next";
 
-export default function ProductPageDetails() {
+export default function ProductPage() {
     return (
-        <div>
-            <h2>ProductPageDetails Page Placeholder</h2>
-        </div>
+        <>
+            <Divider className="mb-0" />
+            <Container>
+                <div className="pt-8">
+                    <Breadcrumb />
+                </div>
+                {/* <ProductSingleDetails /> */}
+                {/* <RelatedProducts sectionHeading="text-related-products" /> */}
+                <Subscription />
+            </Container>
+        </>
     );
 }
+ProductPage.Layout = Layout;
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale!, [
+                "common",
+                "forms",
+                "menu",
+                "footer",
+            ])),
+        },
+    };
+};
