0ef99cc7111e8421a38ec3d7d67dc95e6794274e
php1301 <19520854@gm.uit.edu.vn>
2021-08-01 22:43:32 +0700

    Update API endpoint

--- src/framework/basic-rest/utils/api-endpoints.ts
+++ src/framework/basic-rest/utils/api-endpoints.ts
@@ -12,7 +12,7 @@ export const API_ENDPOINTS = {
     TOP_SELLER_PRODUCTS: "/products_top_seller.json",
     ON_SELLING_PRODUCTS: "/products_on_selling.json",
     PRODUCT: "/api/product.json",
-    RELATED_PRODUCTS: "/related_products.json",
+    RELATED_PRODUCTS: "/api/related_products.json",
     BEST_SELLER_PRODUCTS: "/products_best_seller.json",
     NEW_ARRIVAL_PRODUCTS: "/products_new_arrival.json",
     FLASH_SALE_PRODUCTS: "/products_flash_sale.json",
