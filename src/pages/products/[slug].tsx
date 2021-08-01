9ba425726ddbb91351a78398597b1cbd0774cfc6
php1301 <19520854@gm.uit.edu.vn>
2021-08-01 22:41:51 +0700

    Relative flex-grow for flex child

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -2,7 +2,7 @@ import Container from "@components/ui/container";
 import { Layout } from "@components/layout/layout";
 import { Subscription } from "@components/common/subscription";
 import ProductSingleDetails from "@components/product/product-single-details";
-// import RelatedProducts from "@containers/related-products";
+import RelatedProducts from "@containers/related-products";
 import Divider from "@components/ui/divider";
 import Breadcrumb from "@components/common/breadcrumb";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
@@ -11,18 +11,18 @@ import AssuranceBlock from "@containers/assurance-block";
 
 export default function ProductPage() {
     return (
-        <>
+        <div className="relative flex-grow">
             <Divider className="mb-0" />
             <Container>
                 <div className="pt-8">
                     <Breadcrumb />
                 </div>
                 <ProductSingleDetails />
-                {/* <RelatedProducts sectionHeading="text-related-products" /> */}
+                <RelatedProducts sectionHeading="text-related-products" />
                 <AssuranceBlock />
                 <Subscription />
             </Container>
-        </>
+        </div>
     );
 }
 ProductPage.Layout = Layout;
