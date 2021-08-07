3c01b46fdf91c1d1047c11bea5498bf6ba746705
php1301 <19520854@gm.uit.edu.vn>
2021-08-07 22:56:56 +0700

    Align items section header

--- src/containers/related-products.tsx
+++ src/containers/related-products.tsx
@@ -19,7 +19,10 @@ const RelatedProducts: React.FC<ProductsProps> = ({
 
     return (
         <div className={className}>
-            <SectionHeader sectionHeading={sectionHeading} />
+            <SectionHeader
+                sectionHeading={sectionHeading}
+                justify={"justify-center"}
+            />
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-x-3 md:gap-x-5 xl:gap-x-7 gap-y-3 xl:gap-y-5 2xl:gap-y-8">
                 {error ? (
                     <div className="col-span-full">
