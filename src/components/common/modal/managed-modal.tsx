77964f8680b0283e49cfae1871205718c2bbd8ef
php1301 <19520854@gm.uit.edu.vn>
2021-08-13 22:52:22 +0700

    Newsletter and Forget Password page added

--- src/components/common/modal/managed-modal.tsx
+++ src/components/common/modal/managed-modal.tsx
@@ -1,12 +1,12 @@
 import { useUI } from "@contexts/ui.context";
 import Modal from "./modal";
 import dynamic from "next/dynamic";
-// import Newsletter from "../newsletter";
+import Newsletter from "../newsletter";
 const LoginForm = dynamic(() => import("@components/auth/login-form"));
 const SignUpForm = dynamic(() => import("@components/auth/sign-up-form"));
-// const ForgetPasswordForm = dynamic(
-// 	() => import("@components/auth/forget-password-form")
-// );
+const ForgetPasswordForm = dynamic(
+    () => import("@components/auth/forget-password-form"),
+);
 // const ProductPopup = dynamic(() => import("@components/product/product-popup"));
 
 const ManagedModal: React.FC = () => {
@@ -15,9 +15,9 @@ const ManagedModal: React.FC = () => {
         <Modal open={displayModal} onClose={closeModal}>
             {modalView === "LOGIN_VIEW" && <LoginForm />}
             {modalView === "SIGN_UP_VIEW" && <SignUpForm />}
-            {/* {modalView === "FORGET_PASSWORD" && <ForgetPasswordForm />}
-            {modalView === "PRODUCT_VIEW" && <ProductPopup />} */}
-            {/* {modalView === "NEWSLETTER_VIEW" && <Newsletter />} */}
+            {modalView === "FORGET_PASSWORD" && <ForgetPasswordForm />}
+            {modalView === "NEWSLETTER_VIEW" && <Newsletter />}
+            {/* {modalView === "PRODUCT_VIEW" && <ProductPopup />} */}
         </Modal>
     );
 };
