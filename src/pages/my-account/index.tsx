f33794c2e8d164222cdb67cbcd20800b351fcf5a
php1301 <19520854@gm.uit.edu.vn>
2021-08-14 23:59:41 +0700

    User Account dashboard page added

--- src/pages/my-account/index.tsx
+++ src/pages/my-account/index.tsx
@@ -1,9 +1,59 @@
-import React from "react";
+import Link from "@components/ui/link";
+import { Layout } from "@components/layout/layout";
+import AccountLayout from "@components/my-account/account-layout";
+import { ROUTES } from "@utils/routes";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { GetStaticProps, NextPage } from "next";
 
-export default function MyAccount() {
+const MyAccount: NextPage & { Layout: typeof Layout } = () => {
+    const { t } = useTranslation("common");
+
     return (
-        <div>
-            <h2>MyAccount Page Placeholder</h2>
-        </div>
+        <AccountLayout>
+            <h2 className="text-lg md:text-xl xl:text-2xl font-bold text-heading mb-3 xl:mb-5">
+                {t("text-dashboard")}
+            </h2>
+            <p className=" text-sm leading-7 md:text-base md:leading-loose lowercase">
+                {t("text-account-dashboard")}{" "}
+                <Link
+                    href={ROUTES.ORDERS}
+                    className="text-heading underline font-semibold"
+                >
+                    {t("text-recent-orders")}
+                </Link>
+                , {t("text-manage-your")}{" "}
+                <Link
+                    href={ROUTES.ACCOUNT_DETAILS}
+                    className="text-heading underline font-semibold"
+                >
+                    {t("text-account-details")}
+                </Link>{" "}
+                {t("text-and")}{" "}
+                <Link
+                    href={ROUTES.CHANGE_PASSWORD}
+                    className="text-heading underline font-semibold"
+                >
+                    {t("text-change-your-password")}
+                </Link>
+                .
+            </p>
+        </AccountLayout>
     );
-}
+};
+
+MyAccount.Layout = Layout;
+export default MyAccount;
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale!, [
+                "common",
+                "forms",
+                "menu",
+                "footer",
+            ])),
+        },
+    };
+};
