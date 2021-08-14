346fd910450cf87a11e776ec4ef40e5ae158f648
php1301 <19520854@gm.uit.edu.vn>
2021-08-15 00:06:18 +0700

    Account Details component for edit or view info

--- src/components/my-account/account-details.tsx
+++ src/components/my-account/account-details.tsx
@@ -1,1 +1,13 @@
-export {};
+import { useState, FC } from "react";
+import AccountEdit from "./account-edit";
+import AccountInfo from "./account-info";
+
+const AccountDetails: FC = () => {
+    const [page, setPage] = useState(1);
+    if (page === 1) {
+        return <AccountInfo setPage={setPage} />;
+    } else {
+        return <AccountEdit setPage={setPage} />;
+    }
+};
+export default AccountDetails;
