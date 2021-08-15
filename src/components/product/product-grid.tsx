428e223d823810583fa9ff497b0c66395ec73275
php1301 <19520854@gm.uit.edu.vn>
2021-08-15 23:03:34 +0700

    Product Grids Modified

--- src/components/product/product-grid.tsx
+++ src/components/product/product-grid.tsx
@@ -1,56 +1,35 @@
-import type { FC, useEffect, useState } from "react";
-import { useTranslation } from "next-i18next";
+import { FC, useState } from "react";
 import { useRouter } from "next/router";
 import ProductCard from "@components/product/product-card";
-import { Button } from "@components/ui/button";
-import ReactPaginate from "react-paginate";
+// import { Button } from "@components/ui/button";
 import ProductFeedLoader from "@components/ui/loaders/product-feed-loader";
-import { useProductsQuery } from "@framework/product/get-all-products";
+// import { useProductsQuery } from "@framework/product/get-all-products";
 import { useProductsPaginationQuery } from "@framework/product/get-all-products-pagination";
 import Pagination from "@components/common/pagination";
 
 interface IProductGridProps {
     className?: string;
+    temp_total: number;
 }
 
-export const ProductGrid: FC<IProductGridProps> = ({ className = "" }) => {
+export const ProductGrid: FC<IProductGridProps> = ({
+    className = "",
+    temp_total,
+}) => {
+    const [pageNum, setPageNum] = useState(1);
     const { query } = useRouter();
 
     const {
         isFetching: isLoading,
         data,
         error,
-    } = useProductsPaginationQuery({ page: 1, ...query });
+    } = useProductsPaginationQuery({ page: pageNum, ...query });
 
-    const { t } = useTranslation("common");
     if (error) return <p>{error.message}</p>;
-    console.log(data);
-
-    // const [pageNum, setPageNum] = useState(1);
-    const onPageChange = (i) => {
-        console.log(i);
-        // setPageNum(i);
-    };
-    // useEffect(() => {
-    //     const offset = (pageNum - 1) * limit;
-    //     console.log(offset);
-    // }, [pageNum]);
-    // const totalPages = Math.ceil(totalRecords / limit);
-    // const range = (from, to, step = 1) => {
-    //     let i = from;
-    //     const range = [];
-
-    //     while (i <= to) {
-    //         range.push(i);
-    //         i += step;
-    //     }
-
-    //     return range;
-    // };
     return (
         <>
             <div
-                className={`grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-x-3 lg:gap-x-5 xl:gap-x-7 gap-y-3 xl:gap-y-5 2xl:gap-y-8 ${className}`}
+                className={`grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-x-3 lg:gap-x-5 xl:gap-x-7 gap-y-3 xl:gap-y-5 2xl:gap-y-8 ${className} transition-opacity duration-300`}
             >
                 {isLoading && !data?.products?.length ? (
                     <ProductFeedLoader limit={20} uniqueKey="search-product" />
@@ -58,9 +37,9 @@ export const ProductGrid: FC<IProductGridProps> = ({ className = "" }) => {
                     data?.products?.map((product) => {
                         return (
                             <ProductCard
-                                key={`product--key${product.id}`}
+                                key={`product--key${product._id}`}
                                 product={product}
-                                variant="grid"
+                                variant="jl"
                             />
                         );
                     })
@@ -78,26 +57,7 @@ export const ProductGrid: FC<IProductGridProps> = ({ className = "" }) => {
                     </Button>
                 )}
             </div> */}
-            <div className="container flex justify-start mx-auto product-paginate">
-                <ReactPaginate
-                    previousLabel={"<"}
-                    previousLinkClassName="inline-flex h-8 w-8 text-gray-600 bg-white border border-solid border-[#cfcfcf] leading-8 mx-1 justify-center items-center cursor-pointer text-sm"
-                    nextLinkClassName="inline-flex h-8 w-8 text-gray-600 bg-white border border-solid border-[#cfcfcf] leading-8 mx-1 justify-center items-center cursor-pointer text-sm"
-                    nextLabel={">"}
-                    breakLabel={"..."}
-                    breakClassName={"break-me"}
-                    pageCount={data?.totalPages?.totalPage}
-                    // limit={10}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={3}
-                    // pageLinkClassName="h-10 px-5 text-gray-600 bg-white border border-r-0 border-gray-600 hover:bg-gray-100"
-                    // activeLinkClassName="h-10 px-5 text-white bg-gray-600 border border-r-0 border-gray-600"
-                    onPageChange={onPageChange}
-                    breakLinkClassName="text-sm inline-flex px-[6px] h-8 w-8 text-gray-600 bg-white leading-8 mx-1 justify-center items-center"
-                    containerClassName={"flex w-1/2 items-center"}
-                    activeClassName={"active"}
-                />
-            </div>
+            <Pagination temp_total={temp_total} setPageNum={setPageNum} />
         </>
     );
 };
