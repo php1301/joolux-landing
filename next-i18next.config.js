b8b7bb631af5546d366425987b713cd2139c240f
php1301 <19520854@gm.uit.edu.vn>
2021-08-17 12:00:36 +0700

    Vietnam translation added

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,8 @@
 const path = require("path");
 module.exports = {
     i18n: {
-        locales: ["en", "de", "es", "ar", "he", "zh", "tr"],
-        defaultLocale: "en",
+        locales: ["en", "de", "es", "ar", "he", "zh", "tr", "vi"],
+        defaultLocale: "vi",
         localeDetection: false,
     },
     localePath: path.resolve("./public/locales"),
