3605b8c65a9107a22e1f6c7de75e83afaf13b273
php1301 <19520854@gm.uit.edu.vn>
2021-08-20 00:17:04 +0700

    Product details query hook added

--- src/framework/basic-rest/product/get-product.tsx
+++ src/framework/basic-rest/product/get-product.tsx
@@ -3,9 +3,13 @@ import http from "@framework/utils/http";
 import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
 import { useQuery } from "react-query";
 
-export const fetchProduct = async (_slug: string) => {
+export const fetchProduct = async (id: string) => {
     const { data } = await http.get(`${API_ENDPOINTS.PRODUCT}`);
-    return data;
+    const { data: data2 } = await http.get(
+        `https://api.joolux-client.ml/admin/products/information?id=${id}`,
+    );
+    console.log(data2);
+    return data2?.product;
 };
 
 export const useProductQuery = (slug: string) => {
