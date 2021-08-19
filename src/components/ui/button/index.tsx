154e1430785f30125aff42ff922b53f0938db76a
php1301 <19520854@gm.uit.edu.vn>
2021-08-20 00:13:06 +0700

    iconCart props added for button jl variant

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -8,6 +8,7 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     type?: "submit" | "reset" | "button";
     loading?: boolean;
     disabled?: boolean;
+    iconCart?: boolean;
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
@@ -19,6 +20,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
             active,
             loading = false,
             disabled = false,
+            iconCart = false,
             ...rest
         } = props;
 
@@ -48,6 +50,52 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
                 disabled={disabled}
                 {...rest}
             >
+                {iconCart && (
+                    <span className="createIconSvgWrapper w-6 h-6 min-w-[24px] min-h-[24px] mr-1">
+                        <svg
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            strokeLinejoin="round"
+                            strokeMiterlimit="1.414"
+                            xmlns="http://www.w3.org/2000/svg"
+                            aria-labelledby="title"
+                            viewBox="0 0 24 24"
+                            preserveAspectRatio="xMidYMid meet"
+                            className="createIconSvg"
+                        >
+                            <g fill="none">
+                                <path
+                                    d="M8.25 20.25C8.66421 20.25 9 19.9142 9 19.5C9 19.0858 8.66421 18.75 8.25 18.75C7.83579 18.75 7.5 19.0858 7.5 19.5C7.5 19.9142 7.83579 20.25 8.25 20.25Z"
+                                    stroke="currentColor"
+                                    strokeWidth="1.5"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                                <path
+                                    d="M18.75 20.25C19.1642 20.25 19.5 19.9142 19.5 19.5C19.5 19.0858 19.1642 18.75 18.75 18.75C18.3358 18.75 18 19.0858 18 19.5C18 19.9142 18.3358 20.25 18.75 20.25Z"
+                                    stroke="currentColor"
+                                    strokeWidth="1.5"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                                <path
+                                    d="M2.25 3.75H5.25L7.5 16.5H19.5"
+                                    stroke="currentColor"
+                                    strokeWidth="1.5"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                                <path
+                                    d="M7.5 13.5H19.1925C19.2792 13.5001 19.3633 13.4701 19.4304 13.4151C19.4975 13.3601 19.5434 13.2836 19.5605 13.1986L20.9105 6.44859C20.9214 6.39417 20.92 6.338 20.9066 6.28414C20.8931 6.23029 20.8679 6.18009 20.8327 6.13717C20.7975 6.09426 20.7532 6.05969 20.703 6.03597C20.6528 6.01225 20.598 5.99996 20.5425 6H6"
+                                    stroke="currentColor"
+                                    strokeWidth="1.5"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </g>
+                        </svg>
+                    </span>
+                )}
                 {children}
                 {loading && (
                     <svg
