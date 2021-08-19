569e6c15bb2d72f7e454a69cc26144b2beaa87db
php1301 <19520854@gm.uit.edu.vn>
2021-08-20 00:13:45 +0700

    get related products query hook added

--- src/framework/basic-rest/product/get-related-products.tsx
+++ src/framework/basic-rest/product/get-related-products.tsx
@@ -1,18 +1,28 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { QueryOptionsType, Product } from "@framework/types";
+import { RelatedProductsQueryOptionsType, Product } from "@framework/types";
 import http from "@framework/utils/http";
 import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
 import { useQuery } from "react-query";
 
-export const fetchRelatedProducts = async ({ queryKey }: any) => {
-    const [_key, _params] = queryKey;
+export const fetchRelatedProducts = async (
+    category: string,
+    brand: string,
+    id: string,
+) => {
     const { data } = await http.get(API_ENDPOINTS.RELATED_PRODUCTS);
-    return data;
+    const { data: data2 } = await http.get(
+        `https://api.joolux-client.ml/admin/products/related?category=${category}&brand=${brand}&id=${id}`,
+    );
+    console.log(data2, "related");
+    return data2;
 };
 
-export const useRelatedProductsQuery = (options: QueryOptionsType) => {
+export const useRelatedProductsQuery = (
+    options: RelatedProductsQueryOptionsType,
+) => {
     return useQuery<Product[], Error>(
         [API_ENDPOINTS.RELATED_PRODUCTS, options],
-        fetchRelatedProducts,
+        async () =>
+            fetchRelatedProducts(options.category, options.brand, options.id),
     );
 };
