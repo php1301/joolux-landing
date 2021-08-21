ebae3ba5009b31b8d24320fc93d95d6375ab5d96
php1301 <19520854@gm.uit.edu.vn>
2021-08-22 00:06:33 +0700

    Checkout cart item image nad price formatting

--- src/components/checkout/checkout-card-item.tsx
+++ src/components/checkout/checkout-card-item.tsx
@@ -5,23 +5,29 @@ import usePrice from "@framework/product/use-price";
 export const CheckoutItem: React.FC<{ item: Item }> = ({ item }) => {
     const { price } = usePrice({
         amount: item.itemTotal,
-        currencyCode: "USD",
+        baseAmount: item.itemTotal,
+        currencyCode: "VND",
     });
     return (
         <div className="flex py-4 items-center border-b border-gray-300">
             <div className="flex border rounded-md border-gray-300 w-16 h-16 flex-shrink-0">
                 <img
-                    src={item.image ?? "/assets/placeholder/order-product.svg"}
+                    src={
+                        (item?.image &&
+                            `${process.env.NEXT_PUBLIC_BASE_IMAGE}${item?.image}`) ??
+                        "/assets/placeholder/cart-item.svg"
+                    }
                     width="64"
                     height="64"
                     className="object-cover"
                 />
             </div>
             <h6 className="text-sm ps-3 font-regular text-heading">
-                {generateCartItemName(item.name, item.attributes)}
+                {generateCartItemName(item.name, item.attributes)}{" "}
+                <span className="text-[#101010] text-base font-semibold">{`X ${item.quantity}`}</span>
             </h6>
             <div className="flex ms-auto text-heading text-sm ps-2 flex-shrink-0">
-                {price}
+                {price}&nbsp;₫
             </div>
         </div>
     );
