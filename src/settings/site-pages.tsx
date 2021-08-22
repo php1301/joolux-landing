5d74473757d5a28b54d36beab51630fa3ec02499
php1301 <19520854@gm.uit.edu.vn>
2021-08-22 23:22:02 +0700

    Static site page seo added

---
+++ src/settings/site-pages.tsx
@@ -0,0 +1,44 @@
+export const sitePages = {
+    "hang-moi-ve": {
+        page_title: "Hàng mới về | Joolux",
+        page_description: "Thương hiệu được các tín đồ thời trang yêu thích",
+        banner_title_id: "hangMoiVeId",
+        banner_description_id: "hangMoiVeSubtitle",
+    },
+    "tui-xach": {
+        page_title: "Túi xách | Joolux",
+        page_description: "Một chiếc túi sẽ cho người khác biết nhiều về bạn",
+        banner_title_id: "tuiXacTitle",
+        banner_description_id: "tuiXachSubtitle",
+    },
+    "dong-ho-cao-cap": {
+        page_title: "Đồng hồ | Joolux",
+        page_description: "Không chỉ cho biết thời gian, đồng hồ còn cho biết bạn là ai",
+        banner_title_id: "dongHoTitle",
+        banner_description_id: "dongHoSubTitle",
+    },
+    "giay-sneaker": {
+        page_title: "Giày & Sneaker | Joolux",
+        page_description: "Một đôi giày tốt sẽ giúp bạn đi xa",
+        banner_title_id: "giaySneakerTitle",
+        banner_description_id: "giaySneakerSubTitle",
+    },
+    "phu-kien": {
+        page_title: "Phụ kiện | Joolux",
+        page_description: "Một phụ kiện dù nhỏ nhưng góp phần hoàn thiện trang phục của bạn",
+        banner_title_id: "phuKienTitle",
+        banner_description_id: "phuKienSubTitle",
+    },
+    error: {
+        page_title: "Joolux - Trang không tồn tại",
+        page_description: "Trang này không tồn tại",
+        banner_title_id: "errorsTitle",
+        banner_description_id: "errosSubTitle",
+    },
+    medicine: {
+        page_title: "Medicine - PickBazar",
+        page_description: "Medicine Details",
+        banner_title_id: "medicineTitle",
+        banner_description_id: "medicineSubTitle",
+    },
+};
