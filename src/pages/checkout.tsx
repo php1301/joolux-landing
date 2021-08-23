4f1781f950e8d19848f24570abf70bdca72ee710
php1301 <19520854@gm.uit.edu.vn>
2021-08-23 10:52:52 +0700

    Subscription jl heading style added

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -18,17 +18,19 @@ const CheckoutCard = dynamic(
 );
 const Checkout: NextPage & { Layout: typeof Layout } = () => {
     return (
-        <Container>
-            <div className="py-14 xl:py-20 px-0 2xl:max-w-screen-2xl xl:max-w-screen-xl mx-auto flex flex-col md:flex-row w-full">
-                <div className="md:w-full lg:w-3/5 flex  h-full flex-col -mt-1.5">
-                    <CheckoutForm />
+        <>
+            <Container>
+                <div className="py-14 xl:py-20 px-0 2xl:max-w-screen-2xl xl:max-w-screen-xl mx-auto flex flex-col md:flex-row w-full">
+                    <div className="md:w-full lg:w-3/5 flex  h-full flex-col -mt-1.5">
+                        <CheckoutForm />
+                    </div>
+                    <div className="md:w-full lg:w-2/5 md:ms-7 lg:ms-10 xl:ms-14 flex flex-col h-full -mt-1.5">
+                        <CheckoutCard />
+                    </div>
                 </div>
-                <div className="md:w-full lg:w-2/5 md:ms-7 lg:ms-10 xl:ms-14 flex flex-col h-full -mt-1.5">
-                    <CheckoutCard />
-                </div>
-            </div>
-            <Subscription />
-        </Container>
+            </Container>
+            <Subscription hasTitle />
+        </>
     );
 };
 Checkout.Layout = Layout;
