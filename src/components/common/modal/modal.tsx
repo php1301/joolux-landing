f527d9a50f64ef5e31b030c563ce64894466702b
php1301 <19520854@gm.uit.edu.vn>
2021-08-31 10:38:39 +0700

    Modal z-index fix

--- src/components/common/modal/modal.tsx
+++ src/components/common/modal/modal.tsx
@@ -91,7 +91,7 @@ const Modal: FC<ModalProps> = ({
                             onClick={onClose}
                             aria-label="Close panel"
                             className={cn(
-                                "fixed z-10 inline-flex items-center justify-center w-7 h-7 md:w-8 md:h-8 rounded-full bg-white shadow text-gray-600 transition duration-200 focus:outline-none focus:text-gray-800 focus:shadow-md hover:text-gray-800 hover:shadow-md",
+                                "fixed z-50 inline-flex items-center justify-center w-7 h-7 md:w-8 md:h-8 rounded-full bg-white shadow text-gray-600 transition duration-200 focus:outline-none focus:text-gray-800 focus:shadow-md hover:text-gray-800 hover:shadow-md",
                                 closeBtnClasses[variant],
                             )}
                         >
