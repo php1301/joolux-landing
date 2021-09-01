dc50ea6e4df0bc7c2ade02b2ae7970da22b4d868
php1301 <19520854@gm.uit.edu.vn>
2021-09-01 11:04:07 +0700

    Hotfix facebook login

--- src/pages/connect/facebook-connect.tsx
+++ src/pages/connect/facebook-connect.tsx
@@ -1,12 +1,9 @@
-import React, { useRef, useEffect, useState } from "react";
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
+import React from "react";
+import FacebookLogin from "@greatsumini/react-facebook-login";
 import { useFacebookAuthMutation } from "@framework/auth/use-facebook-auth";
-import router from "next/router";
 
 function FacebookConnect() {
-    let authFbRef = useRef(null);
     const { mutate: authFacebook } = useFacebookAuthMutation();
-    const [sdkLoaded, setSdkLoaded] = useState(false);
     async function handleFacebookAuth(e) {
         console.log(e);
         authFacebook({
@@ -15,31 +12,17 @@ function FacebookConnect() {
             name: e?.name,
             tokenId: e?.accessToken,
         });
-
     }
-    useEffect(() => {
-        if (sdkLoaded) {
-            authFbRef.current.onClick();
-        }
-    }, [sdkLoaded]);
     return (
         <FacebookLogin
             appId={process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_KEY}
-            callback={handleFacebookAuth}
-            // autoLoad={true}
+            onSuccess={handleFacebookAuth}
+            autoLoad={true}
             fields="name,email,picture"
             scope="public_profile,email"
-            onFailure={handleFacebookAuth}
-            render={(renderProps) => {
-                console.log(renderProps);
-                authFbRef.current = renderProps;
-                setSdkLoaded(renderProps.isSdkLoaded);
-                return (
-                    <div
-                    // ref={(button) => (authFbRef.current = button)}
-                    // onClick={renderProps.onClick}
-                    ></div>
-                );
+            onFail={handleFacebookAuth}
+            style={{
+                color: "transparent",
             }}
         />
     );
