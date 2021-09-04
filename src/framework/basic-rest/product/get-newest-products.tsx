17253566acc4aaabe950bb5cc1f2b7aa5627886a
php1301 <19520854@gm.uit.edu.vn>
2021-09-04 23:12:33 +0700

    added homepage, collection, popup banner

--- src/framework/basic-rest/product/get-newest-products.tsx
+++ src/framework/basic-rest/product/get-newest-products.tsx
@@ -1,18 +1,38 @@
-import { Product } from "@framework/types";
+import {
+    CollectionBanner,
+    HomepageBanner,
+    PopupBanner,
+    Product,
+} from "@framework/types";
 import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
 import http from "@framework/utils/http";
 import { useQuery } from "react-query";
 
 type NewestProducts = {
-    data: Product[];
+    newestProducts: Product[];
+    homepageBanner: HomepageBanner[];
+    collectionBanner: CollectionBanner[];
+    popupBanner: PopupBanner;
 };
 
 const fetchNewestProducts = async () => {
-    const { data } = await http.get(
+    const { data: newestProducts } = await http.get(
         `https://api.joolux-client.ml${API_ENDPOINTS.NEW_ARRIVAL_PRODUCTS}`,
     );
+    const { data: homepageBanner } = await http.get(
+        `https://api.joolux-client.ml${API_ENDPOINTS.HOMEPAGE_BANNER}`,
+    );
+    const { data: collectionBanner } = await http.get(
+        `https://api.joolux-client.ml${API_ENDPOINTS.COLLECTION_BANNER}`,
+    );
+    const { data: popupBanner } = await http.get(
+        `https://api.joolux-client.ml${API_ENDPOINTS.POPUP_BANNER}`,
+    );
     return {
-        data,
+        newestProducts,
+        homepageBanner,
+        collectionBanner,
+        popupBanner,
     };
 };
 
