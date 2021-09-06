c5e8bbbd583d46c157f3d2611c8319766f292994
php1301 <19520854@gm.uit.edu.vn>
2021-09-07 00:38:21 +0700

    Checkout clean container page

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -19,7 +19,7 @@ const CheckoutCard = dynamic(
 const Checkout: NextPage & { Layout: typeof Layout } = () => {
     return (
         <>
-            <Container>
+            <Container clean>
                 <div className="py-14 xl:py-20 px-0 2xl:max-w-screen-2xl xl:max-w-screen-xl mx-auto flex flex-col md:flex-row w-full">
                     <div className="md:w-full lg:w-3/5 flex  h-full flex-col -mt-1.5">
                         <CheckoutForm />
