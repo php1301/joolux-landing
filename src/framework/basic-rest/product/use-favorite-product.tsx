903b5d58ca6880c95981addabe8f4a79337c3f72
php1301 <19520854@gm.uit.edu.vn>
2021-09-10 22:02:20 +0700

    Refetch if favorites when not authorized

--- src/framework/basic-rest/product/use-favorite-product.tsx
+++ src/framework/basic-rest/product/use-favorite-product.tsx
@@ -1,26 +1,35 @@
 // import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
 // import http from "@framework/utils/http";
 import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
+import { useUI } from "@contexts/ui.context";
 import http from "@framework/utils/http";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 
 export interface IFavoriteForm {
     product: string;
 }
 async function favoriteProduct(input: IFavoriteForm) {
-    return http.post(
+    return await http.post(
         `https://api.joolux-client.ml${API_ENDPOINTS.FAVORITE}`,
         input,
     );
 }
 
 export const useFavoriteProductMutation = () => {
-    return useMutation((input: IFavoriteForm) => favoriteProduct(input), {
-        onSuccess: (data) => {
-            console.log(data, "FavoriteProduct success response");
+    const { favoriteData } = useUI();
+    const queryClient = useQueryClient();
+    return useMutation(
+        async (input: IFavoriteForm) => await favoriteProduct(input),
+        {
+            onSuccess: async (data) => {
+                if (favoriteData)
+                    await queryClient.resetQueries({
+                        active: true,
+                    });
+            },
+            onError: (data) => {
+                console.log(data, "FavoriteProduct error response");
+            },
         },
-        onError: (data) => {
-            console.log(data, "FavoriteProduct error response");
-        },
-    });
+    );
 };
