1f26c2d6901ac1bc4abfa0662ea9766a21bff1b5
php1301 <19520854@gm.uit.edu.vn>
2021-09-13 21:24:25 +0700

    account details to pass info to AccountEdit

--- src/components/my-account/account-details.tsx
+++ src/components/my-account/account-details.tsx
@@ -1,13 +1,15 @@
+import { UserProfile } from "@framework/types";
 import { useState, FC } from "react";
 import AccountEdit from "./account-edit";
 import AccountInfo from "./account-info";
 
 const AccountDetails: FC = () => {
     const [page, setPage] = useState(1);
+    const [details, setDetails] = useState<UserProfile>();
     if (page === 1) {
-        return <AccountInfo setPage={setPage} />;
+        return <AccountInfo setPage={setPage} setDetails={setDetails} />;
     } else {
-        return <AccountEdit setPage={setPage} />;
+        return <AccountEdit setPage={setPage} details={details} />;
     }
 };
 export default AccountDetails;
