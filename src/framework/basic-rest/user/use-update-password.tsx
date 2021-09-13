174b4e9c3ce12874b9a32f8fae21ed04222fc767
php1301 <19520854@gm.uit.edu.vn>
2021-09-13 22:58:07 +0700

    use update password init

--- src/framework/basic-rest/user/use-update-password.tsx
+++ src/framework/basic-rest/user/use-update-password.tsx
@@ -1,6 +1,8 @@
-// import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
-// import http from "@framework/utils/http";
+import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
+import http from "@framework/utils/http";
+import { toast } from "react-toastify";
 import { useMutation } from "react-query";
+import { useWindowSize } from "@utils/use-window-size";
 
 export interface ChangePasswordInputType {
     newPassword: string;
@@ -8,18 +10,42 @@ export interface ChangePasswordInputType {
     confirmNewpassword: string;
 }
 async function changePassword(input: ChangePasswordInputType) {
-    // return http.post(API_ENDPOINTS.ChangePassword, input);
-    return input;
+    return http.put(
+        `https://api.joolux-client.ml${API_ENDPOINTS.UPDATE_USER_PASSWORD}`,
+        input,
+    );
 }
 export const useChangePasswordMutation = () => {
+    const { width } = useWindowSize();
+
     return useMutation(
         (input: ChangePasswordInputType) => changePassword(input),
         {
             onSuccess: (data) => {
                 console.log(data, "ChangePassword success response");
+                toast("Cập nhật mật khẩu thành công", {
+                    type: "dark",
+                    progressClassName: "fancy-progress-bar",
+                    position: width > 768 ? "bottom-right" : "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                });
             },
             onError: (data) => {
                 console.log(data, "ChangePassword error response");
+                toast("Cập nhật thông tin không thành công", {
+                    type: "error",
+                    progressClassName: "fancy-progress-bar",
+                    position: width > 768 ? "bottom-right" : "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                });
             },
         },
     );
