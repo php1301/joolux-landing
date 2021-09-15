e6e73d91c3703de06bc826821e01cd2209a13976
php1301 <19520854@gm.uit.edu.vn>
2021-09-15 13:39:52 +0700

    ve-chung-toi team section added

---
+++ src/containers/about/team.tsx
@@ -0,0 +1,195 @@
+import React, { FC } from "react";
+
+const Team: FC = () => {
+    return (
+        <div>
+            <div className="mx-auto max-w-[1234px] pt-8 border-t border-[#cfcfcf] font-body other-page">
+                <div className="max-w-[900px] mx-auto">
+                    <h5 className="text-2xl text-center mb-2 text-primary">
+                        Đội Ngũ
+                    </h5>
+                    <div
+                        className="text-gray-700 text-15px mx-auto mb-16 text-center"
+                        style={{ maxWidth: 700 }}
+                    >
+                        Chúng tôi là những người có cùng đam mê với thị trường
+                        hàng hiệu, cùng nhau thực hiện sứ mệnh đưa hàng hiệu đến
+                        gần hơn với người Việt Nam. Chúng tôi tin rằng, bạn xứng
+                        đáng được chiêm ngưỡng, cảm nhận và tận hưởng diện mạo
+                        hoàn hảo nhất của bản thân.
+                    </div>
+                    <div className="about-team-container flex flex-wrap -mx-3 justify-center">
+                        <div className="w-1/2 sm:w-1/3 px-3 mb-16">
+                            <div className="about-team-member-wrapper">
+                                <img
+                                    src="https://joolux.com/img/team/ta-hien.jpg"
+                                    className="rounded-full"
+                                    alt="ta-hien"
+                                />
+                                <div
+                                    className="mb-3 mt-4 border-t-2 border-[#101010]"
+                                    style={{ width: 80 }}
+                                />
+                                <div className="text-xl mb-1 font-semibold">
+                                    Tạ Hiển
+                                </div>
+                                <div className="text-sm text-gray-700">
+                                    CEO &amp; Founder
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-1/2 sm:w-1/3 px-3 mb-16">
+                            <div className="about-team-member-wrapper">
+                                <img
+                                    src="https://joolux.com/img/team/han-ngo.jpg"
+                                    className="rounded-full"
+                                    alt="han-ngo"
+                                />
+                                <div
+                                    className="mb-3 mt-4 border-t-2 border-[#101010]"
+                                    style={{ width: 80 }}
+                                />
+                                <div className="text-xl mb-1 font-semibold">
+                                    Ngô Hân
+                                </div>
+                                <div className="text-sm text-gray-700">CTO</div>
+                            </div>
+                        </div>
+                        <div className="w-1/2 sm:w-1/3 px-3 mb-16">
+                            <div className="about-team-member-wrapper">
+                                <img
+                                    src="https://joolux.com/img/team/nguyen-uyen.jpg"
+                                    className="rounded-full"
+                                    alt="nguyen-uyen"
+                                />
+                                <div
+                                    className="mb-3 mt-4 border-t-2 border-[#101010]"
+                                    style={{ width: 80 }}
+                                />
+                                <div className="text-xl mb-1 font-semibold">
+                                    Nguyễn Uyên
+                                </div>
+                                <div className="text-sm text-gray-700">
+                                    Operation Manager
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-1/2 sm:w-1/3 px-3 mb-16">
+                            <div className="about-team-member-wrapper">
+                                <img
+                                    src="https://joolux.com/img/team/jennifer-do.jpg"
+                                    className="rounded-full"
+                                    alt="jennifer-do"
+                                />
+                                <div
+                                    className="mb-3 mt-4 border-t-2 border-[#101010]"
+                                    style={{ width: 80 }}
+                                />
+                                <div className="text-xl mb-1 font-semibold">
+                                    Jennifer Đỗ
+                                </div>
+                                <div className="text-sm text-gray-700">CMO</div>
+                            </div>
+                        </div>
+                        <div className="w-1/2 sm:w-1/3 px-3 mb-16">
+                            <div className="about-team-member-wrapper">
+                                <img
+                                    src="https://joolux.com/img/team/nguyen-duy.jpg"
+                                    className="rounded-full"
+                                    alt="nguyen-duy"
+                                />
+                                <div
+                                    className="mb-3 mt-4 border-t-2 border-[#101010]"
+                                    style={{ width: 80 }}
+                                />
+                                <div className="text-xl mb-1 font-semibold">
+                                    Nguyễn Duy
+                                </div>
+                                <div className="text-sm text-gray-700">
+                                    Head of Hanoi Office
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-1/2 sm:w-1/3 px-3 mb-16">
+                            <div className="about-team-member-wrapper">
+                                <img
+                                    src="https://joolux.com/img/team/phan-minh.jpg"
+                                    className="rounded-full"
+                                    alt="phan-minh"
+                                />
+                                <div
+                                    className="mb-3 mt-4 border-t-2 border-[#101010]"
+                                    style={{ width: 80 }}
+                                />
+                                <div className="text-xl mb-1 font-semibold">
+                                    Phan Minh
+                                </div>
+                                <div className="text-sm text-gray-700">
+                                    Sale Manager
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-1/2 sm:w-1/3 px-3 mb-16">
+                            <div className="about-team-member-wrapper">
+                                <img
+                                    src="https://joolux.com/img/team/nghiem-duc.jpg"
+                                    className="rounded-full"
+                                    alt="nghiem-duc"
+                                />
+                                <div
+                                    className="mb-3 mt-4 border-t-2 border-[#101010]"
+                                    style={{ width: 80 }}
+                                />
+                                <div className="text-xl mb-1 font-semibold">
+                                    Nghiêm Đức
+                                </div>
+                                <div className="text-sm text-gray-700">
+                                    Business Analyst Manager
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-1/2 sm:w-1/3 px-3 mb-16">
+                            <div className="about-team-member-wrapper">
+                                <img
+                                    src="https://joolux.com/img/team/le-khai.jpg"
+                                    className="rounded-full"
+                                    alt="le-khai"
+                                />
+                                <div
+                                    className="mb-3 mt-4 border-t-2 border-[#101010]"
+                                    style={{ width: 80 }}
+                                />
+                                <div className="text-xl mb-1 font-semibold">
+                                    Lê Khải
+                                </div>
+                                <div className="text-sm text-gray-700">
+                                    Product Manager
+                                </div>
+                            </div>
+                        </div>
+                        <div className="w-1/2 sm:w-1/3 px-3 mb-16">
+                            <div className="about-team-member-wrapper">
+                                <img
+                                    src="https://joolux.com/img/team/la-hung.jpg"
+                                    className="rounded-full"
+                                    alt="la-hung"
+                                />
+                                <div
+                                    className="mb-3 mt-4 border-t-2 border-[#101010]"
+                                    style={{ width: 80 }}
+                                />
+                                <div className="text-xl mb-1 font-semibold">
+                                    Lã Hùng
+                                </div>
+                                <div className="text-sm text-gray-700">
+                                    Financial Manager
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Team;
