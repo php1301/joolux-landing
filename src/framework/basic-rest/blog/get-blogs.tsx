d3f0d552a72720ac699315b8e682d662c5b1c16a
php1301 <19520854@gm.uit.edu.vn>
2021-09-30 00:07:10 +0700

    Get blogs hook added

--- src/framework/basic-rest/blog/get-blogs.tsx
+++ src/framework/basic-rest/blog/get-blogs.tsx
@@ -1,4 +1,4 @@
-import { Blog, Order } from "@framework/types";
+import { Blog } from "@framework/types";
 import http from "@framework/utils/http";
 import { API_ENDPOINTS } from "@framework/utils/api-endpoints";
 import { useQuery } from "react-query";
@@ -11,21 +11,24 @@ type PaginatedBlogs = {
         total: number;
         totalPage: number;
     };
+    seo?: { name: string; slug: string; description: string; image: string };
 };
 
 export const fetchBlogs = async (options) => {
     const {
-        data: { blogs, pagination },
+        data: { blogs, pagination, seo },
     } = await http.get(`https://api.joolux-client.ml${API_ENDPOINTS.BLOG}`, {
         params: {
             page: options?.page || "1",
+            category: options?.blogCategory,
+            seo: true,
         },
     });
-    return { blogs, pagination };
+    return { blogs, pagination, seo: seo || {} };
 };
 export const useGetBlogsQuery = (options) => {
     return useQuery<PaginatedBlogs, Error>(
-        [API_ENDPOINTS.BLOG],
+        [API_ENDPOINTS.BLOG, options],
         () => fetchBlogs(options),
         { keepPreviousData: true },
     );
