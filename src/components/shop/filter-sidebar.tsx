c658800d9395968f0c2329d6c240f6773891e8a9
php1301 <19520854@gm.uit.edu.vn>
2021-10-01 00:07:29 +0700

    cart-button timeout time modified

--- src/components/shop/filter-sidebar.tsx
+++ src/components/shop/filter-sidebar.tsx
@@ -6,7 +6,7 @@ import { useTranslation } from "next-i18next";
 import { getDirection } from "@utils/get-direction";
 import { useRouter } from "next/router";
 
-const FilterSidebar = ({ filter, totalItems }) => {
+const FilterSidebar = ({ filter, totalItems, filterName }) => {
     const { closeFilter } = useUI();
     const router = useRouter();
     const { t } = useTranslation("common");
@@ -32,12 +32,18 @@ const FilterSidebar = ({ filter, totalItems }) => {
 
             <Scrollbar className="menu-scrollbar flex-grow mb-auto">
                 <div className="flex flex-col py-7 px-5 md:px-7 text-heading">
-                    <ShopFilters filter={filter} modalTooltip mobile={false} />
+                    <ShopFilters
+                        filter={filter}
+                        modalTooltip
+                        mobile={false}
+                        totalItems={totalItems}
+                        filterName={filterName}
+                    />
                 </div>
             </Scrollbar>
 
             <div className="text-sm md:text-base leading-4 flex items-center justify-center px-7 flex-shrink-0 h-14 bg-heading text-white">
-                {totalItems} {t("text-items")}
+                {/* {totalItems} {t("text-items")} */}
             </div>
         </div>
     );
