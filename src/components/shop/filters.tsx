515306e9e927459fd0ae878ef9dedfa8a85d9392
php1301 <19520854@gm.uit.edu.vn>
2021-10-01 00:06:06 +0700

    dynamic filterName filters

--- src/components/shop/filters.tsx
+++ src/components/shop/filters.tsx
@@ -23,12 +23,14 @@ interface IShopFilters {
     modalTooltip?: boolean;
     mobile?: boolean;
     totalItems?: number;
+    filterName?: string;
 }
 export const ShopFilters: React.FC<IShopFilters> = ({
     filter,
     modalTooltip = false,
     totalItems = 2000,
     mobile = true,
+    filterName = "Hàng mới về",
 }) => {
     const queryClient = useQueryClient();
     const router = useRouter();
@@ -54,30 +56,30 @@ export const ShopFilters: React.FC<IShopFilters> = ({
             <div className="block border-b border-gray-300 pb-7 mb-7">
                 <div className="flex items-center justify-between mb-2.5">
                     <h2 className="font-semibold text-heading text-xl md:text-2xl">
-                        {t("Hàng mới về")}
+                        {t(filterName)}
                     </h2>
                     <br />
-                    {(!isEmpty(restQuery) || category) && (
-                        <button
-                            className="flex-shrink text-xs mt-0.5 transition duration-150 ease-in focus:outline-none hover:text-heading"
-                            aria-label="Clear All"
-                            onClick={async () => {
-                                const url = query.category
-                                    ? ROUTES.HANG_MOI_VE
-                                    : asPath.split("?")[0];
-                                router.push(url);
-                                await queryClient.resetQueries({
-                                    active: true,
-                                });
-                            }}
-                        >
-                            {t("text-clear-all")}
-                        </button>
-                    )}
                 </div>
-                <div className="flex-shrink-0 text-black text-xs md:text-sm leading-4 pe-4 md:me-6 hidden lg:block mb-4">
+                <div className="flex-shrink-0 text-black text-xs md:text-sm leading-4 pe-4 md:me-6 lg:block mb-3">
                     {totalItems} {t("text-items")}
                 </div>
+                {(!isEmpty(restQuery) || category) && (
+                    <button
+                        className="flex-shrink text-xs mt-0.5 transition duration-150 ease-in focus:outline-none text-heading font-semibold"
+                        aria-label="Clear All"
+                        onClick={async () => {
+                            const url = query.category
+                                ? ROUTES.HANG_MOI_VE
+                                : asPath.split("?")[0];
+                            router.push(url);
+                            await queryClient.resetQueries({
+                                active: true,
+                            });
+                        }}
+                    >
+                        {t("text-clear-filter")}
+                    </button>
+                )}
                 <div className="flex flex-wrap -m-1.5 pt-2">
                     {!isEmpty(restQuery) &&
                         Object.values(restQuery)
@@ -96,26 +98,34 @@ export const ShopFilters: React.FC<IShopFilters> = ({
                             ))}
                 </div>
             </div>
-            {categories && <CategoryFilter categoriesFilter={categories} />}
+            {categories?.length > 0 && (
+                <CategoryFilter categoriesFilter={categories} />
+            )}
             {subCategories?.subCateData?.length > 0 && (
                 <SubCategoryFilter subCategoryFilter={subCategories} />
             )}
-            {brands && <BrandFilter brandsFilter={brands} />}
-            {genders && <GenderFilter gendersFilter={genders} />}
-            {statuses && <StatusFilter statusesFilter={statuses} />}
-            {conditions && (
+            {brands?.length > 0 && <BrandFilter brandsFilter={brands} />}
+            {genders?.length > 0 && <GenderFilter gendersFilter={genders} />}
+            {statuses?.length > 0 && <StatusFilter statusesFilter={statuses} />}
+            {conditions?.length > 0 && (
                 <ConditionFilter
                     conditionsFilter={conditions}
                     modalTooltip={modalTooltip}
                     mobile={mobile}
                 />
             )}
-            {materials && <MaterialFilter materialsFilter={materials} />}
-            {sizes && <SizeFilter sizesFilter={sizes} />}
-            {promotions && <PromotionFilter promotionsFilter={promotions} />}
-            {specials && <SpecialFilter specialsFilter={specials} />}
-            {prices && <PriceFilter pricesFilter={prices} />}
-            {colors && <ColorFilter colorsFilter={colors} />}
+            {materials?.length > 0 && (
+                <MaterialFilter materialsFilter={materials} />
+            )}
+            {sizes?.length > 0 && <SizeFilter sizesFilter={sizes} />}
+            {promotions?.length > 0 && (
+                <PromotionFilter promotionsFilter={promotions} />
+            )}
+            {specials?.length > 0 && (
+                <SpecialFilter specialsFilter={specials} />
+            )}
+            {prices?.length > 0 && <PriceFilter pricesFilter={prices} />}
+            {colors?.length > 0 && <ColorFilter colorsFilter={colors} />}
         </div>
     );
 };
