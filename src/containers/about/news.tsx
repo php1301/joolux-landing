5bab51fb5f95ca41ee397ba120eb30d7e9f864f0
php1301 <19520854@gm.uit.edu.vn>
2021-10-01 22:26:28 +0700

    Fix news block

--- src/containers/about/news.tsx
+++ src/containers/about/news.tsx
@@ -1,28 +1,51 @@
 import React, { FC } from "react";
 import { useWindowSize } from "@utils/use-window-size";
 import Carousel from "@components/ui/carousel/carousel";
+import Image from "next/image";
+import { toBase64 } from "@utils/toBase64";
+import { shimmer } from "@utils/shimmer";
 import { SwiperSlide } from "swiper/react";
-import { newsCarousel } from "@framework/static/news";
+// import { newsCarousel } from "@framework/static/news";
 const breakpoints = {
     "1500": {
         slidesPerView: 4,
     },
-    "700": {
-        slidesPerView: 3,
+    "640": {
+        slidesPerView: 4,
     },
     "0": {
-        slidesPerView: 2,
+        slidesPerView: 4,
     },
 };
 function getImage(deviceWidth: number, imgObj: any) {
-    return deviceWidth < 480 ? imgObj?.mobile : imgObj?.desktop;
+    return deviceWidth < 640
+        ? { url: imgObj.mobile.url, width: 145, height: 60 }
+        : { url: imgObj.desktop.url, width: 205, height: 95 };
 }
 
-const News: FC = () => {
+const News: FC<{
+    news: {
+        id: number;
+        title: string;
+        slug: string;
+        image: {
+            mobile: {
+                url: string;
+                width: number;
+                height: number;
+            };
+            desktop: {
+                url: string;
+                width: number;
+                height: number;
+            };
+        };
+    }[];
+}> = ({ news }) => {
     const { width } = useWindowSize();
     return (
         <div className="border-t border-[#cfcfcf] ">
-            <div className="mx-auto max-w-[1234px] py-10 sm:py-16 font-body">
+            <div className="mx-auto max-w-[1234px] py-10 sm:py-16 font-body overflow-hidden px-5">
                 <div className="flex flex-wrap">
                     <div className="w-full md:w-1/3 lg:w-1/4 md:pr-6 lg:pr-14">
                         <h5
@@ -46,42 +69,56 @@ const News: FC = () => {
                                 clickable: true,
                             }}
                         >
-                            {newsCarousel?.map((news) => (
-                                <SwiperSlide
-                                    className="carouselItem px-0 2xl:px-3.5 h-[104px] w-[226px]"
-                                    key={`banner--key-${news?.id}`}
-                                >
-                                    <div>
-                                        <a
-                                            href={news.slug}
-                                            target="_blank"
-                                            rel="noreferrer noopener"
-                                            className="bg-gray-100 py-2 px-4 xl:py-5 xl:px-8 h-[104px] w-[226px]"
-                                            tabIndex={-1}
-                                            style={{
-                                                width: "100%",
-                                                display: "inline-block",
-                                            }}
-                                        >
-                                            <div className="flex h-full">
-                                                <img
-                                                    // width={226}
-                                                    // height={104}
-                                                    src={
-                                                        getImage(
-                                                            width,
-                                                            news.image,
-                                                        )?.url ||
-                                                        "/assets/images/hero/banner-2-jl.jpg"
-                                                    }
-                                                    className="m-auto"
-                                                    alt={news.title}
-                                                />
-                                            </div>
-                                        </a>
-                                    </div>
-                                </SwiperSlide>
-                            ))}
+                            {news?.map((news) => {
+                                const selectedImage = getImage(
+                                    width,
+                                    news.image,
+                                );
+                                return (
+                                    <SwiperSlide
+                                        className="carouselItem px-0 2xl:px-3.5 smJl:h-[104px] smJl:w-[226px] w-[105px] h-[60px]"
+                                        key={`banner--key-${news?.id}`}
+                                    >
+                                        <div>
+                                            <a
+                                                href={news.slug}
+                                                target="_blank"
+                                                rel="noreferrer noopener"
+                                                className="bg-gray-100 py-2 px-4 xl:py-5 xl:px-8 "
+                                                tabIndex={-1}
+                                                style={{
+                                                    width: "100%",
+                                                    display: "inline-block",
+                                                }}
+                                            >
+                                                <div className="flex h-full">
+                                                    <Image
+                                                        width={
+                                                            selectedImage.width
+                                                        }
+                                                        height={
+                                                            selectedImage.height
+                                                        }
+                                                        placeholder="blur"
+                                                        blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                                                            shimmer(
+                                                                selectedImage.width,
+                                                                selectedImage.height,
+                                                            ),
+                                                        )}`}
+                                                        src={
+                                                            selectedImage?.url ||
+                                                            "/assets/images/hero/banner-2-jl.jpg"
+                                                        }
+                                                        className="m-auto"
+                                                        alt={news.title}
+                                                    />
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </SwiperSlide>
+                                );
+                            })}
                         </Carousel>
                     </div>
                 </div>
