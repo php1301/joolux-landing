fcbe5ca6b9fb66700ea1432e651a8c4fad485cba
php1301 <19520854@gm.uit.edu.vn>
2021-10-08 12:51:47 +0700

    Upload build file

---
+++ .next/server/chunks/262.js
@@ -0,0 +1,110 @@
+"use strict";
+exports.id = 262;
+exports.ids = [262];
+exports.modules = {
+
+/***/ 6262:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5282);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9297);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+
+function ContionTooltip({
+  mobile
+}) {
+  return !mobile ? /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+    className: "bg-white min-w-[31.25rem] ml-5 z-50",
+    style: {
+      boxShadow: "rgb(61 61 61 / 25%) 0px 4px 16px"
+    },
+    children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+      className: "m-auto p-5 relative",
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "text-sm",
+        children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+          className: "flex py-1",
+          children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+            className: "font-bold mr-1 min-w-[110px]",
+            children: "Perfect:"
+          }), "Ch\u01B0a qua s\u1EED d\u1EE5ng, full box"]
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+          className: "flex py-1",
+          children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+            className: "font-bold mr-1 min-w-[110px]",
+            children: "Excellent:"
+          }), "Kh\xF4ng c\xF3 d\u1EA5u hi\u1EC7u h\u01B0 t\u1ED5n ho\u1EB7c c\xF3 d\u1EA5u hi\u1EC7u h\u01B0 t\u1ED5n kh\xF4ng th\u1EA5y r\xF5"]
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+          className: "flex py-1",
+          children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+            className: "font-bold mr-1 min-w-[110px]",
+            children: "Very good:"
+          }), "C\xF3 d\u1EA5u hi\u1EC7u h\u01B0 t\u1ED5n kh\xF4ng \u0111\xE1ng k\u1EC3 do qu\xE1 tr\xECnh s\u1EED d\u1EE5ng"]
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+          className: "flex py-1",
+          children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+            className: "font-bold mr-1 min-w-[110px]",
+            children: "Good:"
+          }), "C\xF3 d\u1EA5u hi\u1EC7u h\u01B0 t\u1ED5n th\u1EA5y r\xF5 do qu\xE1 tr\xECnh s\u1EED d\u1EE5ng"]
+        }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+          className: "flex py-1",
+          children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+            className: "font-bold mr-1 min-w-[110px]",
+            children: "Pending:"
+          }), "\u0110ang x\xE9t duy\u1EC7t t\xECnh tr\u1EA1ng"]
+        })]
+      })
+    })
+  }) : /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+    className: "m-auto p-5 relative",
+    children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+      className: "text-sm",
+      children: [/*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "flex py-1",
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+          className: "font-bold mr-1 min-w-[110px]",
+          children: "Perfect:"
+        }), "Ch\u01B0a qua s\u1EED d\u1EE5ng, full box"]
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "flex py-1",
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+          className: "font-bold mr-1 min-w-[110px]",
+          children: "Excellent:"
+        }), "Kh\xF4ng c\xF3 d\u1EA5u hi\u1EC7u h\u01B0 t\u1ED5n ho\u1EB7c c\xF3 d\u1EA5u hi\u1EC7u h\u01B0 t\u1ED5n kh\xF4ng th\u1EA5y r\xF5"]
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "flex py-1",
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+          className: "font-bold mr-1 min-w-[110px]",
+          children: "Very good:"
+        }), "C\xF3 d\u1EA5u hi\u1EC7u h\u01B0 t\u1ED5n kh\xF4ng \u0111\xE1ng k\u1EC3 do qu\xE1 tr\xECnh s\u1EED d\u1EE5ng"]
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "flex py-1",
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+          className: "font-bold mr-1 min-w-[110px]",
+          children: "Good:"
+        }), "C\xF3 d\u1EA5u hi\u1EC7u h\u01B0 t\u1ED5n th\u1EA5y r\xF5 do qu\xE1 tr\xECnh s\u1EED d\u1EE5ng"]
+      }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "flex py-1",
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+          className: "font-bold mr-1 min-w-[110px]",
+          children: "Pending:"
+        }), "\u0110ang x\xE9t duy\u1EC7t t\xECnh tr\u1EA1ng"]
+      })]
+    })
+  });
+}
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (ContionTooltip);
+
+/***/ })
+
+};
+;
